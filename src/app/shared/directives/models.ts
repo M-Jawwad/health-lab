40c7d27af3689dabe4c23f58a6a70cba06321380
M-Jawwad <muhammadjawwad333@gmail.com>
2023-06-10 23:00:58 +0500

    Auto-generated commit [ci skip]

--- src/app/shared/directives/models.ts
+++
@@ -1,8 +0,0 @@
-// export type SortColumn = keyof Country | '';
-export type SortDirection = 'asc' | 'desc' | '';
-const rotate: { [key: string]: SortDirection } = { 'asc': 'desc', 'desc': '', '': 'asc' };
-
-export interface SortEvent {
-    column: '';
-    direction: SortDirection;
-}
--- src/app/shared/general-table/formats.ts
+++
@@ -1,9 +0,0 @@
-import * as dateFns from 'date-fns';
-
-export const DATE_FORMAT: any = (v: any, r?: any, c?: any) => (v == null) ? ('N/A') : (typeof(v) === 'string') ? dateFns.format(new Date(v), 'dd-MM-yyyy') : dateFns.format(new Date(v * 1000), 'dd-MM-yyyy');
-export const DATETIME_FORMAT: any = (v: any, r?: any, c?: any) => (v == null) ? ('N/A') : dateFns.format(new Date(v * 1000), 'dd-MM-yyyy, hh:mm');
-
-export const FORMATS: any = {
-    date: DATE_FORMAT,
-    datetime: DATETIME_FORMAT,
-}
